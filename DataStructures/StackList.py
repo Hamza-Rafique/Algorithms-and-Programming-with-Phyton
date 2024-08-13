f5f3759108342936732bc894cb4fe51e7057a22d
Hamza <hamza.rafique@iga.gov.bh>
2024-08-13 13:56:26 +0300

    stacklist done with example

--- DataStructures/StackList.py
+++ DataStructures/StackList.py
@@ -1,6 +1,7 @@
 class StackList:
-    def __init__(self):
-        self.items = []
+        
+    # def __init__(self):
+    items = [1, 2, 3, 4, 5]
 
     def isEmpty(self):
         return self.items == []
@@ -12,8 +13,29 @@ class StackList:
         return self.items.pop()
 
     def peek(self):
-        return self.items[len(self.items) - 1]
+         if not self.isEmpty():
+            return self.items[len(self.items) - 1]
+         else:
+            return None
+
+        # return self.items[len(self.items) - 1]
 
     def size(self):
         return len(self.items)
 
+my_stack = StackList()
+
+print(my_stack.isEmpty())
+
+my_stack.push(1)
+my_stack.push(2)
+my_stack.push(3)
+print(my_stack.isEmpty())
+
+print(my_stack.peek())
+print(my_stack.size())
+
+my_stack.pop()
+
+print(my_stack.peek())
+print(my_stack.items)
