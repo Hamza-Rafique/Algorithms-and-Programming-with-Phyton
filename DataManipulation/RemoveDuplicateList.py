87087be7949b91af739a484ed044c53b89bc0569
Hamza <hamza.rafique@iga.gov.bh>
2024-08-28 14:03:36 +0300

    Remove Dupliacted added

---
+++ DataManipulation/RemoveDuplicateList.py
@@ -0,0 +1,11 @@
+def RemoveDuplicateList(input_list):
+    output_list = []
+    for item in input_list:
+        if item not in output_list:
+            output_list.append(item)
+    return output_list
+
+input_list = [1, 2, 3, 4, 5, 3, 2, 6, 7, 8, 1]
+
+
+print(RemoveDuplicateList(input_list))
